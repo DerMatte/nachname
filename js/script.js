e9422ad4ebcb10bd3b40c721f135dbe1aa78ddd6
Matthias <m@mattelap.fritz.box>
2019-04-02 22:50:06 +0200

    new bg color and new Style

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// const element =  document.querySelector('.pad')
+// element.classList.add('animated', 'rotateOut')
+
+// element.addEventListener('animationend', function() { 
+
+//     const ele =  document.querySelector('.headline')
+//     ele.classList.add('animated', 'zoomInUP')
+//  })
